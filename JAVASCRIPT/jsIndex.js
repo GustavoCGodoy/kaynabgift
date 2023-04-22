7ded66c02bdf2453b2aed186d356291cfd5e8873
GustavoCGogoy <gustavo.godoy0502@gmail.com>
2023-04-22 00:06:27 -0300

    index finalizado

---
+++ JAVASCRIPT/jsIndex.js
@@ -0,0 +1,29 @@
+
+function mudarTexto(){
+    document.getElementById('tirar').innerHTML='';
+    document.getElementById('tirar1').innerHTML='';
+    document.getElementById('Mudar1').innerHTML='Meu lugar favorito é você, um lugar repleto de mistérios e segredos.';
+    document.getElementById('Mudar1').style.opacity = 1;
+    document.getElementById('Mudar2').innerHTML='Mas, para aqueles que se aventuram além de conversas rasas e banais, há um mundo a ser descoberto.';
+    document.getElementById('Mudar2').style.opacity = 1;
+    document.getElementById('Mudar3').innerHTML=' Embora seja uma pessoa complexa, Kayná tem uma beleza indescritível. Sua energia radiante brilha no céu, criando um cenário espetacular digno de um pôr do Sol. Ilumina como as estrelas da noite refletem nas dunas com uma luz mágica. E, acima de tudo, há a paz e a tranquilidade da sua calma, onde se pode encontrar um refúgio para a alma.';
+    document.getElementById('Mudar3').style.opacity = 1;
+    document.getElementById('Mudar4').innerHTML='Para aqueles que podem chamar Kayná de próxima, é uma pessoa especial e única. É como aventura, descoberta e liberdade. E, embora possa ser difícil às vezes, a força e a determinação dela é inabalável.';
+    document.getElementById('Mudar4').style.opacity = 1;
+    document.getElementById('Mudar5').innerHTML='Você captura minha imaginação, e permanecerá viva na memória mesmo que um dia não nos falemos mais. Para quem a conhece, é mais do que uma simples amizade - é nosso lar, é nosso tesouro.';
+    document.getElementById('Mudar5').style.opacity = 1;
+}
+function opacidade(){
+    setTimeout(delay,1500);
+    document.getElementById('transformar').style.visibility = 'hidden';
+    document.getElementById('tirar').style.opacity = 0;
+    document.getElementById('tirar1').style.opacity = 0;
+    document.getElementById('Mudar1').style.opacity = 0;
+    document.getElementById('Mudar2').style.opacity = 0;
+    document.getElementById('Mudar3').style.opacity = 0;
+    document.getElementById('Mudar4').style.opacity = 0;
+    document.getElementById('Mudar5').style.opacity = 0;
+    setTimeout(mudarTexto, 2000);
+}
+
+function delay(){};
